f1ea33debb2ada4bb2399099d256fb7d3bb6bedb
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-09 15:07:27 +0100

    protractor basic tests passing

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,22 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
+  })
+
+
+  it('finds profiles', function() {
+
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01')
   });
 });
