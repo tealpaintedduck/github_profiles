11b1817f930e58e44bb619e78b2e400366edb9d9
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-10 15:28:58 +0100

    put api key into env

--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -1,5 +1,12 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
   var queryUrl = 'https://api.github.com/search/users';
+
+  var accessToken = $http( {method: 'GET', url: '/key'} );
+  accessToken.then(function(success) {
+    self.token = success.data
+  })
+
+
   return {
     query: function(searchTerm) {
       return $http({
@@ -7,7 +14,7 @@ githubUserSearch.factory('Search', ['$http', function($http) {
         method: 'GET',
         params: {
           'q': searchTerm,
-          'access_token': access_token
+          'access_token': self.token.access_token
         }
       });
     }
--- server.rb
+++ server.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'json'
+
+API_KEY = ENV['GITHUB_API_SECRET']
 
 get '/' do
   send_file 'public/index.html'
+end
+
+get '/key' do
+  { access_token: API_KEY }.to_json
 end
